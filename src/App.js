00be98f672221dab6e2f6e0e0484cc1464c54745
Isaac Kang <isaacjkang@gmail.com>
2017-09-09 01:45:59 -0400

    Add link to etherscan/tx

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 import Web3 from 'web3';
 import contract from 'truffle-contract'
+import Tx from 'ethereumjs-tx'
+
 import Heading from './components/Heading';
 
 import sale from './config/sale.json'
 import token from './config/token.json'
 
-const adTokenAddress = '0x896152feccb4aebf59b38d3d9c2191a94e02d977'
+const tokenAddress = '0x896152feccb4aebf59b38d3d9c2191a94e02d977'
 const saleAddress = '0xf3dc29ef78c83154c29358eb17e40bfac1b3ed22'
 
 let web3;
@@ -34,8 +36,9 @@ class App extends Component {
     this.state = {
       account: '',
       amount: 300000,
-      ethBalance: '-',
-      adtBalance: '-',
+      ethBalance: '',
+      adtBalance: '',
+      txHash: ''
     }
   }
 
@@ -51,7 +54,7 @@ class App extends Component {
   }
 
   fetchAdtBalance = () => {
-    Token.at(adTokenAddress).then(ins => {
+    Token.at(tokenAddress).then(ins => {
       return ins.balanceOf.call(this.state.account)
     }).then(bal => {
       this.setState({
@@ -76,29 +79,53 @@ class App extends Component {
 
   handleSubmit = (e) => {
     e.preventDefault();
-    Sale.at(saleAddress).then(ins => {
+
+    Token.at(tokenAddress).then(ins => {
       console.log('instance:', ins);
+
       
-    })
+    }).then(result => {
+      console.log('res', result);
+      this.setState({
+        txHash: result
+      });
+    }).catch(error => this.callback(error, 'no result'))
+  }
+
+  callback = (err, result) => {
+    if (err) {
+      console.log('error:', err);
+    } else {
+      console.log('result:', result);
+      this.setState({
+        txHash: result
+      })
+    }
   }
 
   render() {
+    const tx = (
+      <div>
+        <div>Your transaction:</div>
+        <a target="_blank" href={`https://rinkeby.etherscan.io/tx/${this.state.txHash}`}>{this.state.txHash}</a>
+      </div>
+    )
     return (
       <div className="App">
         <Heading address={this.state.account} />
 
-        <br/>
+        <br />
 
         <div onClick={this.fetchAdtBalance}>Click to see ADT Balance:</div>
         <div>{this.state.adtBalance}</div>
 
-        <br/>
+        <br />
 
         <div onClick={this.fetchEthBalance}>Click to see ETH Balance:</div>
         <div>{this.state.ethBalance}</div>
 
-        <br/>
-        
+        <br />
+
         <form onSubmit={this.handleSubmit}>
           <div>Enter ADT Amount you'd like to purchase (default 300,000):</div>
 
@@ -106,6 +133,10 @@ class App extends Component {
 
           <button>Buy ADT</button>
         </form>
+
+        <br />
+
+        {this.state.txHash && tx}
 
       </div>
     );
