78fe09a975fbb684f7d8bf19de0e0c3818bd244c
Mike Goldin <mike.goldin@consensys.net>
2017-09-12 10:40:10 -0400

    Force https

--- server.js
+++ server.js
@@ -1,21 +1,24 @@
 const express = require("express");
-const cors = require("cors");
-const bodyParser = require("body-parser");
-
 const path = require("path");
+
 const app = express();
-/* server configuration */
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
+
+app.use(function(req, res, next) {
+  if((!req.secure) && (req.get('X-Forwarded-Proto') !== 'https')) {
+      res.redirect('https://' + req.get('Host') + req.url);
+    }
+  else
+    next();
+});
 
 /* static route */
 app.use(express.static(__dirname + "/build"));
 
 /* config for browser history in react */
-app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "index.html")));
+app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "index.html"))); // What does this do?
 
 const server = app.listen(process.env.PORT || 3000, () => {
   const port = server.address().port;
   console.log("app listening on port: ", port);
 });
+
